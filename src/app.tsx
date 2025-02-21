3c5281aff461b0a200c5f67cf288de35d3e58a88
Stirling Hostetter <stirlhoss@proton.me>
2025-02-21 10:25:25 -0600

    progress

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Header from "~/components/Header";
 import "./app.css";
+import Footer from "~/components/Footer";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <>
           <Header />
           <Suspense>{props.children}</Suspense>
+          <Footer />
         </>
       )}
     >
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,10 @@
-import { A } from "@solidjs/router";
-
 export default function Home() {
   return (
-    <main class="bg-gradient-to-tl from-zinc-800 via-zinc-700 to-zinc-800 text-center mx-auto px-20 text-gray-700 p-4">
-      <img src="Silphium-hero.png" class="max-h-200" />
-      <p class="text-gray-200 mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="text-gray-200 my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
-    </main>
-  );
+        <main class="bg-gradient-to-tl from-zinc-800 via-zinc-700 to-zinc-800 p-4">
+            <div class="max-w-screen-lg flex flex-col lg:flex-row text-left m-auto">
+                <img src="Silphium-hero.png" class="max-h-200 max-w-full transition delay-100 duration-200 ease-in hover:rotate-x-15 hover:-rotate-y-15 hover:scale-110" />
+                <div id="hero-text" class="text-7xl text-zinc-200 py-8">Taking Semantics to the Source</div>
+            </div>
+        </main>
+    );
 }
